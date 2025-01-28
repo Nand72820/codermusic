8a8f39cf67133eaf184454acaf794428543b711c
mycreation6666 <gtrs43324@gmail.com>
2025-01-28 22:47:41 +0530

    Update play.py

--- NOBITA/utils/inline/play.py
+++ NOBITA/utils/inline/play.py
@@ -40,25 +40,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "🅢—————————"
+        bar = "💚—————————"
     elif 10 < umm < 20:
-        bar = "—🅞————————"
+        bar = "—❤️————————"
     elif 20 <= umm < 30:
-        bar = "——🅝———————"
+        bar = "——💛———————"
     elif 30 <= umm < 40:
-        bar = "———🅐——————"
+        bar = "———💙——————"
     elif 40 <= umm < 50:
-        bar = "————♡—————"
+        bar = "————💖—————"
     elif 50 <= umm < 60:
-        bar = "—————🅡————"
+        bar = "—————🖤————"
     elif 60 <= umm < 70:
-        bar = "——————🅐———"
+        bar = "——————🧡———"
     elif 70 <= umm < 80:
-        bar = "———————🅙——"
+        bar = "———————🩵——"
     elif 80 <= umm < 95:
-        bar = "————————🅐—"
+        bar = "————————🩷—"
     else:
-        bar = "——————————♡"
+        bar = "——————————💜"
 
     buttons = [
         [
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text=" 𝗝ᴏɪɴ 🤌 ", url="https://t.me/NOBITA_SUPPORT",
+                text="🍁 ᴊᴏɪɴ 🍁", url="https://t.me/NYCreation_Chatzone",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/NOBITA_ALL_BOT",
+                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/CreativeYdv",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -99,10 +99,10 @@ def stream_markup(_, videoid, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝗝ᴏɪɴ 🤌", url="https://t.me/NOBITA_SUPPORT",
+                text="🍁 ᴊᴏɪɴ 🍁", url="https://t.me/NYCreation_Chatzone",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/NOBITA_ALL_BOT",
+                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/CreativeYdv",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
