cc8cabb0f4c61ef3b02a012955920946bccf3880
mycreation6666 <gtrs43324@gmail.com>
2025-01-28 21:35:12 +0530

    Update autoleave.py

--- NOBITA/plugins/misc/autoleave.py
+++ NOBITA/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001511253627
-                                and i.chat.id != -1001511253627
+                                and i.chat.id != -1002321189618
+                                and i.chat.id != -1002321189618
                             ):
                                 if left == 20:
                                     continue
